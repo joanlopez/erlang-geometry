ac9645c9ef859c7ba36d92d370a006c51c3a33a3
jlope2 <joan.lopez@bsc.es>
2016-03-16 14:24:40 +0100

    Added more area operations

--- src/geometry.erl
+++ src/geometry.erl
@@ -15,15 +15,27 @@ tests() ->
 	6.0 = area(triangle, {3, 4, 5}),
 	14529.96758965447 = area(triangle, 123, {150, 231}),
 
+	%% Circle area
+	314.1592653589793 = area(circle, 10),
+
+	%% Ellipse area
+	47.12388980384689 = area(ellipse, {3, 5}),
+
+	%% Parallelogram area
+	200 = area(parallelogram, 20, 10),
+
+	%% Everything works?
 	tests_worked.
 
 
 area(circle, Radius) -> math:pi() * Radius * Radius;
-area(triangle, {Edge1, Edge2, Edge3}) -> (Edge1 + Edge2 + Edge3) / 2.
+area(triangle, {Edge1, Edge2, Edge3}) -> (Edge1 + Edge2 + Edge3) / 2;
+area(ellipse, {Radius1, Radius2}) -> math:pi() * Radius1 * Radius2.
 
 area(rectangle, Width, Height) -> Width * Height;
 area(triangle, Angle, {Edge1, Edge2}) -> ((Edge1 * Edge2) / 2) * math:sin(to_radians(Angle));
-area(triangle, Base, Height) -> (Base * Height) / 2.
+area(triangle, Base, Height) -> (Base * Height) / 2;
+area(parallelogram, Base, Height) -> Base * Height.
 
 to_radians(Degrees) -> Degrees * math:pi() / 180.
 to_degrees(Radians) -> Radians * 180 / math:pi().
